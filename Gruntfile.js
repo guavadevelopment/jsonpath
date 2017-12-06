9c2503631d33d1fd4496d81dc9f63d00cacba7f1
David Ewen <davide@apexperience.com.ai>
2017-12-06 11:14:00 +0800

    remove fs calls so jsonpath can be packed with webpack

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,17 +13,6 @@ module.exports = function (grunt) {
                     alias: {
                         "jsonpath": "./index.js"
                     },
-                    require: [
-                        /**
-                         * When running in Node, we require('./aesprim') and that module takes care of monkey-patching esprima
-                         * using resolve, path finding, etc...
-                         * Anyways, Browserify doesn't support "resolve", so we need to trick the module. We'll actually be
-                         * returning a verbatim, non-modified "esprima" when the code runs require('./aesprim').
-                         * That is ok because we will modify the "esprima" source code right after the bundle process, via
-                         * the postBundleCB callback.
-                         */
-                        ["esprima", {expose: "./aesprim"}]
-                    ],
                     ignore: [
                         'file',
                         'system',
@@ -35,15 +24,7 @@ module.exports = function (grunt) {
                         'JSONSelect',
                         './lib/aesprim.js'
                         //'assert' //can't remove because of lib/index.js,
-                    ],
-                    postBundleCB: function(err, src, next) {
-                        /**
-                         * This is ugly, but we need to make "esprima" understand '@' as a valid character.
-                         * It's either this or bundle a copy of the library with those few bytes of changes.
-                         */
-                        src = src.toString("utf8").replace(/(function isIdentifierStart\(ch\) {\s+return)/m, '$1 (ch == 0x40) || ');
-                        next(err, new Buffer(src, "utf8"));
-                    }
+                    ]
                 }
             }
         },
